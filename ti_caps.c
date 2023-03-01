7e61ef86c2815bd1034295cb10791ef71c3238f5
Chuck Jungmann <chuck@cpjj.net>
2023-03-01 17:42:15 -0600

    Reorder destructor type functions near init function.  Added functions for invoking escape sequence commands.

--- ti_caps.c
+++ ti_caps.c
@@ -62,31 +62,52 @@ int TIV_setup(int count, TIV *tivs[])
    return result==OK;
 }
 
-
-int TIV_get_sequence_from_code(const char **sequence, const char *code)
+/**
+ * @brief Frees sequence memory for each @ref TIV element in array.
+ *
+ * @param "tiv"  Pointer to array of @ref TIV elements, the last member of
+ *               which should be an element whose code member is {0};
+ */
+void TIV_destroy_array(TIV *tiv)
 {
-   int rval = EINVAL;
-   const char *seq = NULL;
-   *sequence = NULL;
-
-   static char less_termcap[] = "LESS_TERMCAP_xx";
-   memcpy(&less_termcap[13], code, 2);
-   seq = getenv(less_termcap);
-   if (!seq)
+   TIV *ptr = tiv;
+   while (!TIV_is_terminator(ptr))
    {
-      static char buff[128];
-      char *area = buff;
-      seq = tgetstr(code, &area);
-   }
+      if (ptr->sequence)
+      {
+         free(ptr->sequence);
+         ptr->sequence = NULL;
+      }
 
-   if (seq)
-   {
-      *sequence = seq;
-      rval = 0;
+      ++ptr;
    }
-
-   return rval;
 }
+
+/**
+ * @brief Free memory from a set of TIV arrays.
+ *
+ * This function is a companion to the @ref TIV_setup function that
+ * allocated memory in a set of TIV arrays to start the program.
+ *
+ * @param "count"   number of elements in the following array
+ * @param "tivs"    pointer to an array of TIV arrays that should
+ *                  be destroyed to free the memory in which the
+ *                  terminfo sequences are saved.
+ */
+void TIV_destroy_arrays(int count, TIV *tivs[])
+{
+   // Destroy array of TIV arrays
+   if (count > 0)
+   {
+      TIV **tiv = tivs;
+      for (int i=0; i<count; ++i)
+      {
+         TIV_destroy_array(*tiv);
+         ++tiv;
+      }
+   }
+}
+
 /**
  * @brief For TIV instance, set sequence member with copy of sequence using malloced buffer.
  * @param "tiv"   TIV instance to set
@@ -191,52 +212,6 @@ void TIV_set_array(TIV *tiv)
       TIV_set(ptr);
       ptr->index = index++;
       ++ptr;
-   }
-}
-
-/**
- * @brief Frees sequence memory for each @ref TIV element in array.
- *
- * @param "tiv"  Pointer to array of @ref TIV elements, the last member of
- *               which should be an element whose code member is {0};
- */
-void TIV_destroy_array(TIV *tiv)
-{
-   TIV *ptr = tiv;
-   while (!TIV_is_terminator(ptr))
-   {
-      if (ptr->sequence)
-      {
-         free(ptr->sequence);
-         ptr->sequence = NULL;
-      }
-
-      ++ptr;
-   }
-}
-
-/**
- * @brief Free memory from a set of TIV arrays.
- *
- * This function is a companion to the @ref TIV_setup function that
- * allocated memory in a set of TIV arrays to start the program.
- *
- * @param "count"   number of elements in the following array
- * @param "tivs"    pointer to an array of TIV arrays that should
- *                  be destroyed to free the memory in which the
- *                  terminfo sequences are saved.
- */
-void TIV_destroy_arrays(int count, TIV *tivs[])
-{
-   // Destroy array of TIV arrays
-   if (count > 0)
-   {
-      TIV **tiv = tivs;
-      for (int i=0; i<count; ++i)
-      {
-         TIV_destroy_array(*tiv);
-         ++tiv;
-      }
    }
 }
 
@@ -398,12 +373,156 @@ int TIV_find_index_by_code(TIV *tiv, const char *code)
    return -1;
 }
 
+/**
+ * @brief Search environment and terminfo database for sequence
+ *        associated with @p code.
+ *
+ * Find escape sequence for a given two-character **termcap** code,
+ * searching environment variables first, then the **terminfo**
+ * database.
+ *
+ * The recognized environment variable names start **LESS_TERMCAP_**,
+ * followed by the two-character **termcap** code.  This feature is
+ * modelled after system utility **less**, serving the same purpose.
+ *
+ * @param "sequence"   pointer to char* in to which a pointer to the
+ *                     sequence value is set.
+ * @param "code"       two-character **termcap** code to be sought
+ *
+ * @return 0 for success, errno value if failed to find the entry.
+ */
+int TIV_get_sequence_from_code(const char **sequence, const char *code)
+{
+   int rval = EINVAL;
+   const char *seq = NULL;
+   *sequence = NULL;
+
+   static char less_termcap[] = "LESS_TERMCAP_xx";
+   memcpy(&less_termcap[13], code, 2);
+   seq = getenv(less_termcap);
+   if (!seq)
+   {
+      static char buff[128];
+      char *area = buff;
+      seq = tgetstr(code, &area);
+   }
+
+   if (seq)
+   {
+      *sequence = seq;
+      rval = 0;
+   }
+
+   return rval;
+}
+
+/**
+ * @brief Simple function to extract the sequence from a TIV variable
+ * @param "tiv"    pointer to a TIV struct
+ * @return pointer to escape sequence, or NULL if no sequence available
+ */
 const char *TIV_get_sequence(const TIV *tiv)
 {
    if (tiv && tiv->sequence)
       return tiv->sequence;
    else
       return NULL;
+}
+
+/**
+ * @brief Simple submission of sequence associated with the indicate TIV element.
+ *
+ * This is not a "safe" function in that it does not confirm a sane
+ * index number (within the range of the array).  If safety is needed,
+ * write a wrapper function that confirms the index.
+ *
+ * @param "tiv"   Array of TIV elements from which the item will be selected
+ * @param "index" Index into array @p tiv.
+ */
+void TIV_execute(const TIV **tiv, int index)
+{
+   const TIV *t = tiv[index];
+   if (t->sequence)
+      tputs(t->sequence, 1, putchar);
+}
+
+/**
+ * @brief Same as @ref TIV_execute, but includes a lines-affected parameter.
+ *
+ * Unlike @ref TIV_execute, this function sets the "affcnt" argument of
+ * @p tputs to indicated affected lines.  Mostly for scrolling functions,
+ * but may be necessary for other capabilities also.
+ *
+ * @param "tiv"       Array of TIV elements from which the item will be selected
+ * @param "index"     Index into array @p tiv.
+ * @param "linecount" Number of lines to be affected by the call.
+ */
+void TIV_execute_with_lines(const TIV **tiv, int index, int linecount)
+{
+   const TIV *t = tiv[index];
+   if (t->sequence)
+      tputs(t->sequence, linecount, putchar);
+}
+
+/**
+ * @brief Simple submission of sequence with arguments.
+ *
+ * This is not a "safe" function in that it does not confirm a sane
+ * index number (within the range of the array).  If safety is needed,
+ * write a wrapper function that confirms the index.
+ *
+ * @param "tiv"   Array of TIV elements from which the item will be selected
+ * @param "index" Index into array @p tiv.
+ * @param "..."   const int arguments as necessary for the indicated capability.
+ */
+void TIV_execute_params(const TIV **tiv, int index,...)
+{
+   const TIV *t = tiv[index];
+   if (t->sequence)
+   {
+      va_list list_args;
+      va_start(list_args, index);
+      int arg1 = va_arg(list_args, int);
+      int arg2 = va_arg(list_args, int);
+      int arg3 = va_arg(list_args, int);
+      int arg4 = va_arg(list_args, int);
+      int arg5 = va_arg(list_args, int);
+      va_end(list_args);
+
+      const char *str = tiparm(t->sequence, arg1, arg2, arg3, arg4, arg5);
+      tputs(str, 1, putchar);
+   }
+}
+
+/**
+ * @brief Simple submission of sequence with arguments with lines-affected parameter.
+ *
+ * This is not a "safe" function in that it does not confirm a sane
+ * index number (within the range of the array).  If safety is needed,
+ * write a wrapper function that confirms the index.
+ *
+ * @param "tiv"       Array of TIV elements from which the item will be selected
+ * @param "index"     Index into array @p tiv.
+ * @param "linecount" Number of lines to be affected by the call.
+ * @param "..."       const int arguments as necessary for the indicated capability.
+ */
+void TIV_execute_params_with_lines(const TIV **tiv, int index, int linecount,...)
+{
+   const TIV *t = tiv[index];
+   if (t->sequence)
+   {
+      va_list list_args;
+      va_start(list_args, linecount);
+      int arg1 = va_arg(list_args, int);
+      int arg2 = va_arg(list_args, int);
+      int arg3 = va_arg(list_args, int);
+      int arg4 = va_arg(list_args, int);
+      int arg5 = va_arg(list_args, int);
+      va_end(list_args);
+
+      const char *str = tiparm(t->sequence, arg1, arg2, arg3, arg4, arg5);
+      tputs(str, linecount, putchar);
+   }
 }
 
 #ifdef TI_CAPS_MAIN
--- ti_caps.h
+++ ti_caps.h
@@ -2,6 +2,7 @@
 #define TI_CAPS_H
 
 #include <stddef.h>
+#include <stdarg.h>
 
 typedef struct terminfo_value {
    char code[2];
@@ -14,6 +15,11 @@ int TIV_is_terminator(TIV *tiv);
 // Initialize environment
 int TIV_setup(int count, TIV *tivs[]);
 
+// Deinitialize/Memory recovery
+void TIV_destroy_array(TIV *tiv);
+void TIV_destroy_arrays(int count, TIV *tivs[]);
+
+// Use to populate TIV elements with escape sequences
 int TIV_get_sequence_from_code(const char **sequence, const char *code);
 
 // Setting capability escape sequences
@@ -22,10 +28,6 @@ int TIV_set_from_env(TIV *tiv);
 int TIV_set_from_termcap(TIV *tiv);
 int TIV_set(TIV *tiv);
 void TIV_set_array(TIV *tiv);
-
-// Memory recovery
-void TIV_destroy_array(TIV *tiv);
-void TIV_destroy_arrays(int count, TIV *tivs[]);
 
 // Display/debugging functions
 int TIV_string_array_max_length(const char **strings);
@@ -39,6 +41,11 @@ int TIV_find_index_by_code(TIV *tiv, const char *code);
 
 const char *TIV_get_sequence(const TIV *tiv);
 
+// Using capabilities
+void TIV_execute(const TIV **tiv, int index);
+void TIV_execute_with_lines(const TIV **tiv, int index, int linecount);
+void TIV_execute_params(const TIV **tiv, int index,...);
+void TIV_execute_params_with_lines(const TIV **tiv, int index, int linecount,...);
 
 
 #endif
